d338bc9ed045cacc30d3227e123d19e893c342b3
varsha <varshabalemi@gmail.com>
2024-04-06 17:40:33 +0530

    updated

--- Server/index.js
+++ Server/index.js
@@ -85,7 +85,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
   cors:{
-    origin:"http://localhost:5174",
+    origin:"http://localhost:5173",
     methods:["GET","POST"],
   },
 })
